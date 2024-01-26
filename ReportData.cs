9754178de84d66d362757448ca0ba8ef89008841
jsh <jsh@infobank.net>
2024-01-26 10:14:58 +0900

    optional 필드 추가

--- ReportData.cs
+++ ReportData.cs
@@ -2,8 +2,8 @@ public class ReportData
 {
     public string MessageId { get; set; }
     public string ResultCode { get; set; }
-    public string ErrorText { get; set; }
+    public string? ErrorText { get; set; }
     public string ReportTime { get; set; }
     public string? BrandtalkType { get; set; }
-    public string Ref { get; set; }
+    public string? Ref { get; set; }
 }
